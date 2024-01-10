4d1c7deff284bfb1ded59d05825c9bfea220e1f8
Wyatt Johnson <wyatt.j1834@gmail.com>
2024-01-10 11:31:04 -0700

    Show images

--- resources/js/components/ProductImages.vue
+++ resources/js/components/ProductImages.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="product-images">
-    <div class="product-image">
-      <img :src="activeImage" alt="Product Image" />
-    </div>
-    <div class="row row-wrap">
-      <div
-        class="col-3 col-md-2"
-        v-for="(image, index) in images"
-        :key="index"
-        @click="activeImage = image"
-      >
-        <img class="w-100" :src="image.path" alt="Product Thumbnail" />
-      </div>
-    </div>
+  <img class="w-100 mb-2" :src="activeImage" alt="Product Image" />
+  <div class="d-flex flex-wrap">
+    <img
+      class="thumbnail-image mx-1 mb-1"
+      :class="{ active: activeImage === image.path }"
+      v-for="(image, index) in images"
+      :key="index"
+      @click="activeImage = image.path"
+      :src="image.path"
+      alt="Product Thumbnail"
+    />
   </div>
 </template>
 
@@ -29,5 +26,26 @@ export default {
       activeImage: null,
     };
   },
+  mounted() {
+    this.activeImage = this.images[0].path;
+  },
 };
 </script>
+
+<style scoped>
+.thumbnail-image {
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  object-fit: cover;
+  border: 1px solid #555;
+  border-radius: 2px;
+}
+
+@media screen and (min-width: 768px) {
+  .thumbnail-image {
+    width: 65px;
+    height: 65px;
+  }
+}
+</style>
